1fe5e187fc129149fe8334e3a553bd03626f7f62
Shavkat <shavkatkurbanov065@gmail.com>
2025-03-30 21:30:57 +0500

    sevret malumotlarni .env file ga o'tqazildi

--- core/settings.py
+++ core/settings.py
@@ -13,14 +13,24 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import environ
+import os
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Загружаем .env
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yw)8y%(696b5az54o9se)ef*wj5_t*u!20x!1&cccelm!d^b#('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -231,10 +241,10 @@ LOGOUT_URL = '/admin/logout/'
 LOGIN_REDIRECT_URL = '/'
 
 # Django SMTP
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "storeserver065@gmail.com"
-EMAIL_HOST_PASSWORD = "knxm yrfg zzjm sdan"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_USE_SSL = env("EMAIL_USE_SSL")
